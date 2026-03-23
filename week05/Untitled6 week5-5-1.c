2516f1ded50921e452bcb827bb71e77cb649e7fb
14750792-dot <14750792@me.mcu.edu.tw>
2026-03-23 11:35:40 +0800

    week5

---
+++ week05/Untitled6 week5-5-1.c
@@ -0,0 +1,14 @@
+//week5-5 003
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int ans=0;
+    for (int i=0;i<N;i++){
+        int now;
+        scanf("%d ", &now);
+        ans += now;
+    }
+    printf("%d\n",ans);
+}
