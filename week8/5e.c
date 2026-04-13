da17c64f9dfbce329ff0399c6853f2a2927cc2da
14750792-dot <14750792@me.mcu.edu.tw>
2026-04-13 12:01:51 +0800

    week08
    week08

---
+++ week8/5e.c
@@ -0,0 +1,8 @@
+//5e
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d%d",&a, &b);
+    printf("%d", a*a - b*b);
+}
---
+++ week8/week08-5a.c
@@ -0,0 +1,9 @@
+//5a
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    if (N>2) printf("%d", N-2);
+    else printf("%d", 2-N);
+}
